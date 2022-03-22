b62f17efc6bb9d389195382cab11570155b726e5
eke <ek.e.agafonova@gmai.com>
2022-03-22 21:06:53 +0300

    add barlow_twins_model.py

---
+++ src/barlow_twins_model.py
@@ -0,0 +1,24 @@
+from lightly.models.modules import BarlowTwinsProjectionHead
+from my_resnet import resnet20
+from torch import nn
+from utils import custom_collate_fn, get_classes
+
+
+class BarlowTwins(nn.Module):
+    def __init__(self, backbone):
+        super().__init__()
+        self.backbone = backbone
+        self.projection_head = BarlowTwinsProjectionHead(64, 2048, 512) # orig: (64,2048,2048)
+
+    def forward(self, x):
+        x = self.backbone(x).flatten(start_dim=1)
+        # embedding from backbone ResNet20
+        self.backbone_embedding = x
+        z = self.projection_head(x)
+        # embedding from projection head
+        self.proj_embedding = z
+        return z
+
+resnet = resnet20(num_classes=1) #ResNet20
+backbone = nn.Sequential(*list(resnet.children())[:-1])
+model = BarlowTwins(backbone)
